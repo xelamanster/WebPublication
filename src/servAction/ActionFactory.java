19987852b8d16fd4160eeda1d7484de6e8041058
xelamanster <xelamanster@gmail.com>
2013-06-17 20:39:35 +0300

    some minor changes

--- src/servAction/ActionFactory.java
+++ src/servAction/ActionFactory.java
@@ -4,31 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ActionFactory {
-	Map map=defaultMap();
-	
-	public Action create(String action_name){
-		Class cl=(Class) map.get(action_name);
-		if (cl == null)
-            throw new RuntimeException();
-       Action actionInstance = null;
-       try {
-            actionInstance = (Action)cl.newInstance();
-       } catch (Exception e) {
-            e.printStackTrace();
-       }
+  Map map = defaultMap();
 
-		return actionInstance;
+  public Action create(String action_name) {
+	Class cl = (Class) map.get(action_name);
+	if (cl == null)
+	  throw new RuntimeException();
+	Action actionInstance = null;
+	try {
+	  actionInstance = (Action) cl.newInstance();
+	} catch (Exception e) {
+	  e.printStackTrace();
 	}
-	protected Map defaultMap() {
-		Map map=new HashMap();
-		map.put("addpub", AddPub.class);
-		map.put("addsub", AddSub.class);
-		map.put("adduser", AddUser.class);
-		map.put("deletepub", DeletePub.class);
-		map.put("deletesub", DeleteSub.class);
-		map.put("login", LoginUser.class);
-		map.put("logout", LogOut.class);
-		map.put("paysub", PaySub.class);
-		return map;
-	}
+
+	return actionInstance;
+  }
+
+  protected Map defaultMap() {
+	Map map = new HashMap();
+	map.put("addpub", AddPub.class);
+	map.put("addsub", AddSub.class);
+	map.put("adduser", AddUser.class);
+	map.put("deletepub", DeletePub.class);
+	map.put("deletesub", DeleteSub.class);
+	map.put("login", LoginUser.class);
+	map.put("logout", LogOut.class);
+	map.put("paysub", PaySub.class);
+	return map;
+  }
 }
